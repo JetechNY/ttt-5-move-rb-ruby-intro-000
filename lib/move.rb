1fd2dcde1879136f10fb6d31adb62e09d9dde3fd
SunJet Liu <sunjetliu@gmail.com>
2020-08-07 20:56:27 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,85 +8,85 @@ end
 
 # def input_to_index()
 #   describe '#input_to_index' do
-# 
+#
 #     it 'converts a user_input to an integer' do
 #       user_input = "1"
 #       converted_input = input_to_index(user_input)
-# 
+#
 #       expect(converted_input).to be_a(Integer)
 #     end
-# 
+#
 #     it 'subtracts 1 from the user_input' do
 #       user_input = "6"
 #       converted_input = input_to_index(user_input)
-# 
+#
 #       expect(converted_input).to be(5)
 #     end
-# 
+#
 #     it 'returns -1 for strings without integers' do
 #       user_input = "invalid"
 #       converted_input = input_to_index(user_input)
-# 
+#
 #       expect(converted_input).to be(-1)
 #     end
-# 
+#
 # end
-# 
+#
 # def move()
 #   describe './lib/move.rb' do
 #     it 'defines a move method' do
 #       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #       expect(defined?(move)).to be_truthy
 #     end
-# 
+#
 #     context '#move' do
 #       it 'accepts 3 arguments: the board, the position a player wants to fill and their char, X or O' do
 #         expect{move}.to raise_error(ArgumentError)
 #       end
-# 
+#
 #       it 'provides a default value for the 3rd argument' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         expect {move(board, 2)}.to_not raise_error
 #       end
-# 
+#
 #       it 'allows "X" player in the top left position' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         move(board, 0, "X")
-# 
+#
 #         expect(board).to eq(["X", " ", " ", " ", " ", " ", " ", " ", " "])
 #       end
-# 
+#
 #       it 'allows "O" player in the middle' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         move(board, 4, "O")
-# 
+#
 #         expect(board).to eq([" ", " ", " ", " ", "O", " ", " ", " ", " "])
 #       end
-# 
+#
 #       it 'allows "X" player in the bottom right' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         move(board, 8)
-# 
+#
 #         expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", "X"])
 #       end
-# 
+#
 #       it 'allows "X" player in the bottom right and "O" in the top left ' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         move(board, 0, "O")
 #         move(board, 8, "X")
-# 
+#
 #         expect(board).to eq(["O", " ", " ", " ", " ", " ", " ", " ", "X"])
 #       end
-# 
+#
 #       it 'allows "X" to win diagonally' do
 #         board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #         move(board, 0)
 #         move(board, 4)
 #         move(board, 8)
-# 
+#
 #         expect(board).to eq(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
 #       end
-# 
+#
 #       it 'allows a tie game' do
 #         board = Array.new(9, " ")
 #         move(board, 0, "X")
@@ -98,7 +98,7 @@ end
 #         move(board, 6, "X")
 #         move(board, 7, "X")
 #         move(board, 8, "O")
-# 
+#
 #         expect(board).to eq(["X", "O", "X", "O", "X", "O", "X", "X", "O"])
 #       end
 #     end
